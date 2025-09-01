2444888e09fca15636af9893ad0eb8a952274dda
ronaldleonardo <86000468+ronaldleonardo@users.noreply.github.com>
2025-09-01 19:48:24 +0700

    Added HAM-A

---
+++ src/data/hamaQuestions.jsx
@@ -0,0 +1,407 @@
+export const hamaQuestions = {
+  icon: "😱",
+  colorScheme: {
+    primary: "#E27429", // Light Orange
+    secondary: "#D55209", // Dark Orange
+  },
+  questions: [
+    {
+      id: 1,
+      question: "Mood Cemas",
+      description:
+        "Khawatir, antisipasi hal terburuk, antisipasi dengan rasa takut, iritabel.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "Tekanan",
+      description:
+        "Rasa tertekan, kelesuan, respon terkejut, mudah meneteskan air mata, gemetar, rasa gelisah, tidak bisa bersantai/rileks.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Ketakutan",
+      description:
+        "Terhadap gelap, orang asing, ditinggalkan sendiri, hewan, lalu lintas, keramaian.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Insomnia",
+      description:
+        "Kesulitan masuk tidur, sering terbangun saat tidur, tidur tidak puas dan terasa lelah saat bangun, mimpi/mimpi buruk, terbangun dari tidur dengan ketakutan/terror (night terror).",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Intelektual",
+      description:
+        "Sulit berkonsentrasi, ingatan buruk.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Mood depresi",
+      description:
+        "Kehilangan minat, kurang merasa senang saat menjalankan hobi, depresi, cepat bangun, depresi di pagi hari yang membaik seiring berjalannya hari.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Somatik (Otot)",
+      description:
+        "Nyeri dan sakit, kedutan, kekakuan, sentakan otot (myoclonic jerks), menggertakan/menggesekan gigi, suara goyah, tonus otot meningkat.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Somatik (Sensorik)",
+      description:
+        "Tinitus, pandangan kabur, rasa panas dan dingin tiba-tiba, rasa kelemahan, sensasi tertusuk.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Gejala Kardiovaskular",
+      description:
+        "Takikardi, palpitasi, nyeri dada, denyutan pembuluh darah, rasa mau pingsan, missing beat",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Gejala Pernapasan",
+      description:
+        "Tekanan atau konstriksi di dada, rasa tercekik, menghela napas, sesak napas.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Gejala Gastrointestinal",
+      description:
+        "Sulit menelan, nyeri perut, rasa terbakar, rasa penuh di perut, nausea, vomitus, borborygmi, BAB cair, berat badan turun, konstipasi.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Gejala Genitourinari",
+      description:
+        "Sering BAK, tidak bisa tahan BAK, amenorrhea, menorrhagia, gagal merespon stimulus seksual, ejakulasi dini, libido turun/hilang, impotensi.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Gejala Otonom",
+      description:
+        "Mulut kering, flushing, pucat, cenderung berkeringat, rasa berputar seperti akan jatuh, tension headache, rambut berdiri.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Perilaku saat Wawancara",
+      description:
+        "Membuat gerakan-gerakan kecil, gelisah, atau tidak bisa diam, tremor kedua tangan, mengerutkan alis, wajah tegang, menghela napas atau napas cepat, wajah pucat, sering menelan, dll.",
+      options: [
+        {
+          score: 0,
+          text: "Tidak Ada",
+        },
+        {
+          score: 1,
+          text: "Ringan",
+        },
+        {
+          score: 2,
+          text: "Sedang",
+        },
+        {
+          score: 3,
+          text: "Berat",
+        },
+        {
+          score: 4,
+          text: "Sangat Berat",
+        },
+      ],
+    },
+  ],
+  severityLevels: [
+    { minScore: 0, maxScore: 17, level: "Ringan" },
+    { minScore: 18, maxScore: 24, level: "Ringan-Sedang" },
+    { minScore: 25, maxScore: 30, level: "Sedang-Berat" },
+    { minScore: 30, maxScore: Infinity, level: "Berat" },
+  ],
+};
